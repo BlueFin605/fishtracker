9d2bc9b6cd0bb1be3e8da206869c55c28957f699
BlueFin605 <bluefin605@gmail.com>
2024-06-28 09:53:10 +1200

    create development template

--- angular/src/environments/environment prod.ts
+++ angular/src/environments/environment prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     auth0Domain: '{{DOMAON}}',
     auth0ClientId: '{CLIENTID}'
   };
---
+++ angular/src/environments/environment.development.template.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    auth0Domain: '<<Put your auth0 domain here>>',
+    auth0ClientId: '<<Put yur auth0 clent id here>>'
+  };
