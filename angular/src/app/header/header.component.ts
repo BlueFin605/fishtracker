e64951f7bcbc506649464c2f12bd4f28063220c8
BlueFin605 <bluefin605@gmail.com>
2024-06-27 17:27:26 +1200

    add in user profile

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthButtonComponent } from '../auth-button/auth-button.component';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AuthButtonComponent],
+  imports: [AuthButtonComponent, UserProfileComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
