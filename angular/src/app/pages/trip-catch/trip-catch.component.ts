2b7957ce89fc7a1002bec18bc131c8c1d05f7836
BlueFin605 <bluefin605@gmail.com>
2024-06-29 12:25:07 +1200

    use css for page

--- angular/src/app/pages/trip-catch/trip-catch.component.ts
+++ angular/src/app/pages/trip-catch/trip-catch.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   selector: 'app-trip-catch',
   templateUrl: './trip-catch.component.html',
+  styleUrls: ['./trip-catch.component.css'],
   imports: [CommonModule]
 })
 export class TripCatchComponent implements OnInit {
