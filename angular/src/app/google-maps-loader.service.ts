c31c7f1714d8c7113b85a65f14d34c44fe06c10e
BlueFin605 <bluefin605@gmail.com>
2024-07-23 20:52:32 +1200

    load maps asynchronously

--- angular/src/app/google-maps-loader.service.ts
+++ angular/src/app/google-maps-loader.service.ts
@@ -8,23 +8,43 @@ export class GoogleMapsLoaderService {
 
   private loaded = false;
 
-  constructor() { }
+  private promise: Promise<void> = new Promise<void>(() => {});
+
+  private resolver:any;
+
+
+  constructor() {
+    // Step 1: Define mapInit globally
+    (window as any)['mapInit'] = this.mapInit.bind(this);
+  }
 
   loadScript(): Promise<void> {
-    return new Promise((resolve, reject) => {
+    this.promise = new Promise((resolve, reject) => {
       if (this.loaded) {
         resolve();
         return;
       }
       
+      this.resolver = resolve;
+
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapiApi}`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapiApi}&loading=async&callback=mapInit`;
+      script.async = true; // Ensure the script is loaded asynchronously
       script.onload = () => {
         this.loaded = true;
-        resolve();
       };
       script.onerror = (error) => reject(error);
       document.head.appendChild(script);
     });
+
+    return this.promise;
+  }
+
+  // Step 2: Implement the mapInit function
+  private mapInit() {
+    // Initialize your map or perform other actions here
+    console.log('Google Maps API is loaded and ready to use');
+    this.resolver();
+    // For example, to initialize a map: new google.maps.Map(document.getElementById('map'), options);
   }
 }
