7c5ad5d2aef8351944570312804923cf6dc001ad
BlueFin605 <bluefin605@gmail.com>
2024-08-31 12:41:25 +1200

    remove import

--- FishTrackerLambdaValidator/index.js
+++ FishTrackerLambdaValidator/index.js
@@ -1,61 +1,51 @@
-import { CognitoJwtVerifier } from "aws-jwt-verify";
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
 const lib = require('./lib');
 let data;
 
 // Lambda function index.handler - thin wrapper around lib.authenticate
 module.exports.handler = async (event, context, callback) => {
+    // Extract the token from the Authorization header
+    const token = event.headers.Authorization || event.headers.authorization;
 
-  console.log('lmabda validator')
-  console.log('event')
-  console.log(event)
-  console.log('context')
-  console.log(context)
+    console.log(`token:[${token}]`);
+    if (!token) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({ message: "Unauthorized" }),
+        };
+    }
 
-  const token = event.headers.Authorization || event.headers.authorization;
-  console.log(token);
+    // Verifier that expects valid access tokens:
+    const verifier = CognitoJwtVerifier.create({
+        userPoolId: "eu-central-1_mM4RIUG7b",
+        tokenUse: "access",
+        clientId: "580bdivmu2jc8p09aj8cl8ffid",
+    });
 
-  // Verifier that expects valid access tokens:
-  const verifier = CognitoJwtVerifier.create({
-    userPoolId: "eu-central-1_mM4RIUG7b",
-    tokenUse: "access",
-    clientId: "580bdivmu2jc8p09aj8cl8ffid",
-  });
+    try {
+        const payload = await verifier.verify(token.replace("Bearer ", ""));
+        console.log("Token is valid. Payload:", payload);
+    } catch (err) {
+        console.log("Token not valid!", err);
+        return {
+            statusCode: 401,
+            body: JSON.stringify({ message: "Unauthorized" }),
+        };
+    }
 
-  try {
-    const payload = await verifier.verify(token.replace("Bearer ", ""));
-    console.log("Token is valid. Payload:", payload);
-  } catch (err) {
-    console.log("Token not valid!", err);
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ message: "Unauthorized" }),
-    };
-  }
-
-  // Continue with your logic
-  try {
-    data = await lib.authenticate(event);
-    console.log(data);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (err) {
-    console.log("Error authenticating", err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
-    };
-  }
-  // try {
-  //   console.log(event)
-  //   console.log(context)
-  //   data = await lib.authenticate(event);
-  //   console.log(data)
-  // }
-  // catch (err) {
-  //     console.log(err);
-  //     return context.fail("Unauthorized");
-  // }
-  // return data;
+    // Continue with your logic
+    try {
+        data = await lib.authenticate(event);
+        console.log(data);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (err) {
+        console.log("Error authenticating", err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal Server Error" }),
+        };
+    }
 };
