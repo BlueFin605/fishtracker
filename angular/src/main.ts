8768acf09194a9d59ed052d80cb4f34357aae8d7
BlueFin605 <bluefin605@gmail.com>
2025-01-16 07:46:03 +1300

    added pwa, but has issues

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -5,6 +5,8 @@ import { routes } from './app/app.routes'; // Import the app.routes file
 import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS, withInterceptorsFromDi} from "@angular/common/http";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AuthInterceptor } from './app/services/auth.interceptor';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -14,6 +16,9 @@ bootstrapApplication(AppComponent, {
       ),
       { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch((err) => console.error(err));
